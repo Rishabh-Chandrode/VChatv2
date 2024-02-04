6a19c61fe674a4c6a69ff42ddbfb1ba93801b8e6
rishabh-rapidquest <rishabhc@rapidquest.in>
2024-02-04 18:00:11 +0530

    env configured

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -6,7 +6,7 @@ export const appwriteConfig = {
     storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
     databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
     savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
-    postsCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
+    postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
     userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
 }
 export const client = new Client();
